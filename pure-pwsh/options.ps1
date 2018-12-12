22274127b2c193be7f2fdf8048960b18c0331e30
Nick <nickcox1@gmail.com>
2018-12-12 18:04:12 +1100

    Make PWD and branch name formatting customizable.

--- pure-pwsh/options.ps1
+++ pure-pwsh/options.ps1
@@ -1,66 +1,74 @@
 ﻿function Set-PureOption() {
-  [CmdletBinding()]
-  param (
-    [ValidateSet(
-      'PwdColor',
-      'BranchColor',
-      'RemoteColor',
-      'ErrorColor',
-      'PromptColor',
-      'PromptChar',
-      'UpChar',
-      'DownChar')]
-    $Option,
+    [CmdletBinding()]
+    param (
+        [ValidateSet(
+            'PwdColor',
+            'BranchColor',
+            'RemoteColor',
+            'ErrorColor',
+            'PromptColor',
+            'PromptChar',
+            'UpChar',
+            'DownChar',
+            'SlowCommandTime',
+            'FetchInterval',
+            'BranchFormatter',
+            'PwdFormatter')]
+        $Option,
 
-    [String]
-    $Value
-  )
+        [String]
+        $Value
+    )
 
-  if ($Option -like '*Color') {
-    $Global:pure.$option = (ansiSequence $Value)
-  }
-  else {
-    $Global:pure.$option = $Value
-  }
+    if ($Option -like '*Color') {
+        $Global:pure.$option = (ansiSequence $Value)
+    }
+    else {
+        $Global:pure.$option = $Value
+    }
 }
 
 function ansiSequence([string] $value) {
-  $(if ($value.Contains($esc)) {$value} else {"$esc[$value"}) +
-  "*$esc[0m" # append an asterisk and reset the colour for display purposes
+    $(if ($value.Contains($esc)) {$value} else {"$esc[$value"}) +
+    "*$esc[0m" # append an asterisk and reset the colour for display purposes
 }
 
 function initOptions() {
-  $psrOptions = Get-PSReadlineOption
+    $psrOptions = Get-PSReadlineOption
 
-  if ($psrOptions) {
-    if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'PromptText') {
-      Set-PSReadLineOption -PromptText ("{0} " -f $pure.PromptChar)
-    }
-    if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'ContinuationPrompt') {
-      Set-PSReadLineOption -ContinuationPrompt ("{0}{0} " -f $pure.PromptChar)
+    if ($psrOptions) {
+        if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'PromptText') {
+            Set-PSReadLineOption -PromptText ("{0} " -f $pure.PromptChar)
+        }
+        if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'ContinuationPrompt') {
+            Set-PSReadLineOption -ContinuationPrompt ("{0}{0} " -f $pure.PromptChar)
+        }
+
+        if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'Colors') {
+            Set-PSReadLineOption -Colors @{ ContinuationPrompt = $psrOptions.EmphasisColor }
+        }
+
+        if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'ExtraPromptLineCount') {
+            Set-PSReadLineOption -ExtraPromptLineCount 2
+        }
     }
 
-    if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'Colors') {
-      Set-PSReadLineOption -Colors @{ ContinuationPrompt = $psrOptions.EmphasisColor }
-    }
+    $id = {$input}
 
-    if ((Get-PSReadlineOption).PSObject.Properties.Name -contains 'ExtraPromptLineCount') {
-      Set-PSReadLineOption -ExtraPromptLineCount 2
-    }
-  }
-
-  $Global:pure = New-Object PSObject -Property (
-    [ordered]@{
-      PwdColor        = ansiSequence ("34m")
-      BranchColor     = ansiSequence ("90m")
-      RemoteColor     = ansiSequence ("36m")
-      ErrorColor      = ansiSequence ("91m")
-      PromptColor     = ansiSequence ("35m")
-      PromptChar      = '❯'
-      UpChar          = '⇡'
-      DownChar        = '⇣'
-      SlowCommandTime = [timespan]::FromSeconds(5.0)
-      FetchInterval   = [timespan]::FromSeconds(300)
-      DebounceMs      = 500
-    })
+    $Global:pure = New-Object PSObject -Property (
+        [ordered]@{
+            PwdColor        = ansiSequence ("34m")
+            BranchColor     = ansiSequence ("90m")
+            RemoteColor     = ansiSequence ("36m")
+            ErrorColor      = ansiSequence ("91m")
+            PromptColor     = ansiSequence ("35m")
+            PromptChar      = '❯'
+            UpChar          = '⇡'
+            DownChar        = '⇣'
+            SlowCommandTime = [timespan]::FromSeconds(5.0)
+            FetchInterval   = [timespan]::FromSeconds(300)
+            BranchFormatter = $id
+            PwdFormatter    = $id
+            DebounceMs      = 500
+        })
 }
--- pure-pwsh/prompt.ps1
+++ pure-pwsh/prompt.ps1
@@ -1,86 +1,91 @@
 function global:prompt {
-  $isError = !$?
-  $Script:timer.Stop() # if we have a pending redraw, cancel it now
+    $isError = !$?
+    $Script:timer.Stop() # if we have a pending redraw, cancel it now
 
-  $startTime = Get-Date
+    $startTime = Get-Date
 
-  $hasRepoChanged = !($PWD.Path -eq $watcher.Path -or (
-      $watcher.EnableRaisingEvents -and ($PWD.Path -like "$($watcher.Path)*")))
+    $hasRepoChanged = !($PWD.Path -eq $watcher.Path -or (
+            $watcher.EnableRaisingEvents -and ($PWD.Path -like "$($watcher.Path)*")))
 
-  $curPath = $PWD.Path
-  if ($curPath.ToLower().StartsWith($Home.ToLower())) {
-    $curPath = "~" + $curPath.SubString($Home.Length)
-  }
-
-  $prompt = "`n"
-  $prompt += "$curPath " | fmtColor $pure.pwdColor
-
-  if ((!$maybeDirty -and !$hasRepoChanged -and $promptStatus.gitDir) -or (
-      $gitStatus = if ($null -ne (Get-Module posh-git)) {get-gitstatus} else {$null})) {
-
-    if ($hasRepoChanged -or !$watcher.EnableRaisingEvents) {
-      Log 'Updating watched repo'
-
-      $watcher.Path = git rev-parse --show-toplevel | Resolve-Path
-      $watcher.EnableRaisingEvents = $true
+    $curPath =
+    if ($PWD.Path.StartsWith($Home, [StringComparison]::OrdinalIgnoreCase)) {
+        "~" + $PWD.Path.SubString($Home.Length)
+    }
+    else {
+        $PWD.Path
     }
 
-    if ($gitStatus) { $Script:promptStatus = getPromptStatus $gitStatus }
+    $prompt = "`n"
+    $prompt += "$curPath" | &$pure.PwdFormatter | fmtColor $pure.pwdColor
 
-    if ($pure.FetchInterval -gt 0) { asyncGitFetch }
+    if ((!$maybeDirty -and !$hasRepoChanged -and $promptStatus.gitDir) -or (
+            $gitStatus = if ($null -ne (Get-Module posh-git)) {get-gitstatus} else {$null})) {
 
-    $dirtyMark = if ($promptStatus.isDirty) { "*" } else { "" }
-    $prompt += ($promptStatus.branch + $dirtyMark) | fmtColor $pure.branchColor
-    $prompt += " "
+        if ($hasRepoChanged -or !$watcher.EnableRaisingEvents) {
+            Log 'Updating watched repo'
 
-    if ($promptStatus.isBehind) {
-      $prompt += $pure.downChar | fmtColor $pure.remoteColor
+            $watcher.Path = git rev-parse --show-toplevel | Resolve-Path
+            $watcher.EnableRaisingEvents = $true
+        }
+
+        if ($gitStatus) { $Script:promptStatus = getPromptStatus $gitStatus }
+
+        if ($pure.FetchInterval -gt 0) { asyncGitFetch }
+
+        $dirtyMark = if ($promptStatus.isDirty) { "*" } else { "" }
+        $branchName = $promptStatus.branch | &$pure.BranchFormatter
+ 
+        $prompt += " $branchName$dirtyMark" | fmtColor $pure.branchColor
+        $prompt += " "
+
+        if ($promptStatus.isBehind) {
+            $prompt += $pure.downChar | fmtColor $pure.remoteColor
+        }
+
+        if ($promptStatus.isAhead) {
+            $prompt += $pure.upChar | fmtColor $pure.remoteColor
+        }
     }
 
-    if ($promptStatus.isAhead) {
-      $prompt += $pure.upChar | fmtColor $pure.remoteColor
+    else {
+        $watcher.Path = $PWD
+        $watcher.EnableRaisingEvents = $false
+        $Script:promptStatus = getPromptStatus $null
     }
-  }
 
-  else {
-    $watcher.Path = $PWD
-    $watcher.EnableRaisingEvents = $false
-    $Script:promptStatus = getPromptStatus $null
-  }
-
-  if ($lastCmd = Get-History -Count 1) {
-    $diff = $lastCmd.EndExecutionTime - $lastCmd.StartExecutionTime
-    if ($diff -gt $pure.SlowCommandTime) {
-      $prompt += " ($("{0:f2}" -f $diff.TotalSeconds)s)" | fmtColor $pure.errorColor
+    if ($lastCmd = Get-History -Count 1) {
+        $diff = $lastCmd.EndExecutionTime - $lastCmd.StartExecutionTime
+        if ($diff -gt $pure.SlowCommandTime) {
+            $prompt += " ($("{0:f2}" -f $diff.TotalSeconds)s)" | fmtColor $pure.errorColor
+        }
     }
-  }
 
-  $promptColor = if ($isError) {$pure.errorColor} else {$pure.PromptColor}
-  $prompt += "`n"
-  $prompt += "$($pure.PromptChar) " | fmtColor $promptColor
-  $prompt
+    $promptColor = if ($isError) {$pure.errorColor} else {$pure.PromptColor}
+    $prompt += "`n"
+    $prompt += "$($pure.PromptChar) " | fmtColor $promptColor
+    $prompt
 
-  $Script:maybeDirty = $false
-  $Script:backoff = $pure.DebounceMs
-  $endTime = (Get-Date) - $startTime
-  Log $endTime.TotalMilliseconds
+    $Script:maybeDirty = $false
+    $Script:backoff = $pure.DebounceMs
+    $endTime = (Get-Date) - $startTime
+    Log $endTime.TotalMilliseconds
 }
 
 $emptyStatus = @{
-  HasWorking = $false
-  HasIndex   = $false
-  AheadBy    = 0
-  BehindBy   = 0
+    HasWorking = $false
+    HasIndex   = $false
+    AheadBy    = 0
+    BehindBy   = 0
 }
 
 function getPromptStatus($gitStatus) {
-  $status = $gitStatus |??? $emptyStatus
-  return [ordered]@{
-    updated  = if ($gitStatus) {Get-Date} else {[DateTime]::MinValue}
-    isDirty  = ($status.HasWorking -or $status.HasIndex)
-    isAhead  = ($status.AheadBy -gt 0)
-    isBehind = ($status.BehindBy -gt 0)
-    gitDir   = $status.GitDir
-    branch   = $status.Branch
-  }
+    $status = $gitStatus |??? $emptyStatus
+    return [ordered]@{
+        updated  = if ($gitStatus) {Get-Date} else {[DateTime]::MinValue}
+        isDirty  = ($status.HasWorking -or $status.HasIndex)
+        isAhead  = ($status.AheadBy -gt 0)
+        isBehind = ($status.BehindBy -gt 0)
+        gitDir   = $status.GitDir
+        branch   = $status.Branch
+    }
 }
